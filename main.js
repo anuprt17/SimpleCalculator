cab0c25bee0cda86a1cf34d52492738a20c98b77
Anupreet Kaur <akaur574@myseneca.ca>
2023-05-18 17:35:21 -0400

    final commit

--- main.js
+++ main.js
@@ -6,8 +6,32 @@ function display(value) {
     document.getElementById("result").value += value;
 }
 
-function calculate(){
+/*function calculate(){
     let a = document.getElementById("result").value;
     let b = eval(a);
     document.getElementById("result").value = b;
+}*/
+
+function calculate() {
+    let expression = document.getElementById("result").value;
+    let numbers = expression.split(/[-+*/]/); 
+    let operators = expression.replace(/[0-9.]/g, "").split(""); 
+
+    let result = parseFloat(numbers[0]);
+
+    for (let i = 0; i < operators.length; i++) {
+        let num = parseFloat(numbers[i + 1]);
+
+        if (operators[i] === "+") {
+            result += num;
+        } else if (operators[i] === "-") {
+            result -= num;
+        } else if (operators[i] === "*") {
+            result *= num;
+        } else if (operators[i] === "/") {
+            result /= num;
+        }
+    }
+
+    document.getElementById("result").value = result;
 }
